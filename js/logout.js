a245e449bdfed866443e8e8c34089fd4837981c6
soleylin <soley10160329@gmail.com>
2024-05-31 23:59:01 +0800

    del js

--- js/logout.js
+++
@@ -1,8 +0,0 @@
-import { setCookie } from "./login.js";
-
-$(function () {
-  //監聽 #logout_btn
-  $("#logout_btn").click(function () {
-    setCookie("uid","",7)
-    location.href="https://soleylin.github.io/spa_cat";});
-})
